5cd6782b20624d37bd8f6357c15efe6d9a72bab7
vusause <mikevu123@gmail.com>
2018-04-13 18:01:31 -0400

    Change blog prefix-path

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  pathPrefix: `/vusause.github.io`,
+  pathPrefix: `/blog`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
